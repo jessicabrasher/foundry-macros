9a3a20ddcfc325b6ea05a5abaf85695c7f970382
Jessica Lane <jessica.lynn.brasher@gmail.com>
2022-09-27 10:13:19 -0400

    druid wild shape updates

---
+++ DRUID_RandomWildShape.js
@@ -0,0 +1,7 @@
+let actor;
+let actorName;
+
+let levelTwoBeasts = ["Baboon", "Badger", "Cat", "Deer", "Giant Fire Beetle", "Goat", "Hyena", "Jackal", "Lizard", "Rat", "Scorpion", "Spider", "Weasel", "Camel", "Diseased Giant Rat", "Giant Rat", "Giant Weasel", "Mastiff", "Mule", "Pony", "Axe Beak", "Boar", "Draft Horse", "Elk", "Giant Badger", "Giant Centipede", "Giant Lizard", "Giant Wolf Spider", "Panther", "Riding Horse", "Wolf"];
+let levelFourBeasts = ["Crab", "Frog", "Octopus", "Quipper", "Sea Horse", "Giant Crab", "Poisonous Snake", "Constrictor Snake", "Giant Frog", "Giant Lizard Variant", "Giant Poisonous Snake", "Ape", "Black Bear", "Crocodile", "Giant Goat", "Giant Sea Horse", "Reef Shark", "Warhorse"];
+let levelEightBeasts = ["Bat", "Eagle", "Hawk", "Owl", "Raven", "Vulture", "Blood Hawk", "Flying Snake", "Stirge", "Giant Bat", "Giant Owl", "Pteranodon", "Giant Wasp", "Brown Bear", "Dire Wolf", "Giant Eagle", "Giant Hyena", "Giant Octopus", "Giant Spider", "Giant Toad", "Giant Vulture", "Lion", "Tiger"];
+
